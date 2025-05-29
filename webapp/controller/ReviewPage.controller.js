acee1a21a1780d9d610d43467df9fee9730c5eeb
Elfred Alver Cantuba <elfred0709@gmail.com>
2025-05-29 14:09:01 +0000

    fragments, routing and configuration - 2

--- webapp/controller/ReviewPage.controller.js
+++ webapp/controller/ReviewPage.controller.js
@@ -2,21 +2,25 @@ sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
     "sap/ui/core/routing/History"
-],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller, MessageToast, History) {
+],/**
+* @param {typeof sap.ui.core.mvc.Controller} Controller
+*/
+function (Controller, MessageToast, History) {
+    "use strict";
 
-onPressBack: function () {
-                var oHistory = History.getInstance();
-                var sPreviousHash = oHistory.getPreviousHash();
-                var oRouter = this.getOwnerComponent().getRouter();
+    return Controller.extend("com.training.exer1cantuba.controller.ReviewPage", {
+        onInit() {
+        },
+        onPressBack: function () {
+            var oHistory = History.getInstance();
+            var sPreviousHash = oHistory.getPreviousHash();
+            var oRouter = this.getOwnerComponent().getRouter();
 
-                if (sPreviousHash !== undefined) {
-                    window.history.go(-1);
-                } else {
-                    oRouter.navTo("RouteMainView", {}, true);
-                }
+            if (sPreviousHash !== undefined) {
+                window.history.go(-1);
+            } else {
+                oRouter.navTo("RouteMainView", {}, true);
             }
-        });
+        }
+    });
+});
